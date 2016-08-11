8f7f52eebc93b63ba3e04e52dac2f332e31b8551
Sergey Melnik <serhii.melnyk@TX.local>
2016-08-11 16:33:09 +0300

    Added pub

---
+++ public/1.bund.js
@@ -0,0 +1,19 @@
+webpackJsonp([1],{
+
+/***/ 7:
+/***/ function(module, exports) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	angular.module('app').registerComponent('heroDetail', {
+	    template: 'huyak'
+	});
+
+	exports.default = angular.module('app');
+
+/***/ }
+
+});
---
+++ public/2.bund.js
@@ -0,0 +1,19 @@
+webpackJsonp([2],{
+
+/***/ 7:
+/***/ function(module, exports) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var _module = angular.module('module1', []);
+
+	console.log('module1 ready');
+
+	exports.default = _module;
+
+/***/ }
+
+});
